9b2f5d1c06c86ed5f7adef6277a3b6ec9c32df79
Nico Rithner <nicorithner@gmail.com>
2021-04-03 05:34:23 -0600

    Complete section 'communicating with props' and exercises

--- components/src/index.js
+++ components/src/index.js
@@ -2,28 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail 
-        author="Sam" 
-        image= {faker.image.avatar()} 
-        timeAgo="Today at 4:45PM" 
-        comment= {faker.lorem.sentence()}
-      />
-      <CommentDetail 
-        author="Alex" 
-        image= {faker.image.avatar()} 
-        timeAgo="Today at 5:45PM" 
-        comment= {faker.lorem.sentence()}
-      />
-      <CommentDetail 
-        author="Jane" 
-        image= {faker.image.avatar()} 
-        timeAgo="Today at 3:45PM" 
-        comment= {faker.lorem.sentence()}
-      />
+      <ApprovalCard>
+        <CommentDetail 
+          author="Sam" 
+          image= {faker.image.avatar()} 
+          timeAgo="Today at 4:45PM" 
+          comment= {faker.lorem.sentence()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail 
+          author="Alex" 
+          image= {faker.image.avatar()} 
+          timeAgo="Today at 5:45PM" 
+          comment= {faker.lorem.sentence()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail 
+          author="Jane" 
+          image= {faker.image.avatar()} 
+          timeAgo="Today at 3:45PM" 
+          comment= {faker.lorem.sentence()}
+        />
+      </ApprovalCard>
     </div>
   )
 };
