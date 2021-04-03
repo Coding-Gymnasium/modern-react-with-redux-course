7199be5a8fb679f567b07f46e74f6f158065eff0
Nico Rithner <nicorithner@gmail.com>
2021-04-02 22:28:37 -0600

    Complete passing props exercise and lesson

--- components/src/index.js
+++ components/src/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 import CommentDetail from './CommentDetail';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam"/>
-      <CommentDetail author="Alex"/>
-      <CommentDetail author="Jane"/>
+      <CommentDetail 
+        author="Sam" 
+        image= {faker.image.avatar()} 
+        timeAgo="Today at 4:45PM" 
+        comment= {faker.lorem.sentence()}
+      />
+      <CommentDetail 
+        author="Alex" 
+        image= {faker.image.avatar()} 
+        timeAgo="Today at 5:45PM" 
+        comment= {faker.lorem.sentence()}
+      />
+      <CommentDetail 
+        author="Jane" 
+        image= {faker.image.avatar()} 
+        timeAgo="Today at 3:45PM" 
+        comment= {faker.lorem.sentence()}
+      />
     </div>
   )
 };
