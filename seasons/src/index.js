59aa36d5ba22c8a4e421636d57518be2801527cb
Nico Rithner <nicorithner@gmail.com>
2021-04-05 10:31:17 -0600

    Define constrator method and
    initialize state object

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+  // initialize state with constructor 
+  constructor(props) {
+    super(props);
+    this.state = { lat: null };
+  }
+  // React says we have to define render!!
   render() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => console.log(position),
