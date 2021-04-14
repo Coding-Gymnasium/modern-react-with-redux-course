f7a6102aa8ea9eb30e4f6f01d1664c99758d6ea5
Nico Rithner <nicorithner@gmail.com>
2021-04-14 14:55:24 -0600

    Add defaultProps. Add spinner default message

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -23,7 +23,7 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />;
     }
 
-    return <Spinner message="Please accept location request" />;
+    return <Spinner />;
   }
 }
 
