92ed6cdedc1cb7a77ab52ae6214313c5e103b27d
Nico Rithner <nicorithner@gmail.com>
2021-04-22 22:42:08 -0600

    Correct typos

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -3,9 +3,10 @@ import axios from 'axios';
 const KEY = `${process.env.REACT_APP_YOUTUBE_API_KEY}`;
 
 export default axios.create({
-  baseURL: 'https://ww.googleapis.com/youtube/v3',
+  baseURL: 'https://www.googleapis.com/youtube/v3',
   params: {
-    part: 'snipet',
+    part: 'snippet',
+    type: 'video',
     maxResults: 5,
     key: KEY
   }
