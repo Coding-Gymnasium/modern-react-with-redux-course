88c137d61eed87bfd543d55841a14e513dae0bc6
Nico Rithner <nicorithner@gmail.com>
2021-05-07 12:03:52 -0600

    Update App with Translate component
    Update Dropdown component to have
    a dynamic label

--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 
-const Dropdown = ({ options, selected, onSelectedChange }) => {
+const Dropdown = ({ label, options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false);
   const ref = useRef();
  
@@ -39,7 +39,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
  return (
   <div ref={ref} className='ui form'>
     <div className='field'>
-      <label className='label'> Select a Color</label>
+      <label className='label'> {label} </label>
       <div  
         onClick={() => setOpen(!open)} 
         className={`ui selection dropdown ${open ? 'visible active' : ''}`}
@@ -49,7 +49,6 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
         <div className={`menu ${open ? 'visible transition' : ''}`}>
           {renderedOptions}
         </div>
-        <p style={{color: selected.value}}> This text is {selected.value}</p>
       </div>
     </div>
   </div>
