e6b5d3d54bfee0be97a85b4460b3ae6d04a0d378
Nico Rithner <nicorithner@gmail.com>
2021-05-28 11:30:22 -0600

    Add redux-thunk and set up api call to jsonplaceholder

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,6 +1,8 @@
-export const fetchPosts = () => {
-  return {
-    type: 'Fetch Posts'
-  };
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPosts = () => async (dispatch) => {
+    const response = await jsonPlaceholder.get('/posts');
+
+    dispatch({ type: 'FETCH__POSTS', payload: response });
 };
 
